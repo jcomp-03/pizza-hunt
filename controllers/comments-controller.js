70f2ec141e14dc1908300ded1cd8e48df345f76e
jcomp-03@James-Desktop <jcomp_03@yahoo.com>
2022-11-14 16:56:16 -0500

    Added route for putting new reply to comment, and deleting reply associated with comment. Integrated reply functionality with frontend.

--- controllers/comments-controller.js
+++ controllers/comments-controller.js
@@ -1,57 +1,83 @@
-const { Comment, Pizza } = require('../models');
+const { Comment, Pizza } = require("../models");
 
 const CommentController = {
+  // add comment to pizza
+  addComment({ params, body }, res) {
+    console.log(body);
 
-    // add comment to pizza
-    addComment({ params, body }, res) {
-        console.log(body);
+    Comment.create(body)
+      .then(({ _id }) => {
+        return Pizza.findOneAndUpdate(
+          { _id: params.pizzaId },
+          //When you add data into a nested array of a MongoDB document, they
+          // become what's known as a "nested document" or "subdocument".
+          { $push: { comments: _id } },
+          { new: true }
+        );
+      })
+      .then((dbPizzaData) => {
+        if (!dbPizzaData) {
+          res.status(404).json({ message: "No pizza found with this id!" });
+          return;
+        }
+        res.json(dbPizzaData);
+      })
+      .catch((err) => res.json(err));
+  },
 
-        Comment.create(body)
-            .then(({ _id }) => {
+  // remove comment
+  removeComment({ params }, res) {
+    Comment.findOneAndDelete({
+      _id: params.commentId,
+    })
+      .then((deletedComment) => {
+        if (!deletedComment) {
+          return res.status(404).json({ message: "No comment with this id!" });
+        }
 
-                return Pizza.findOneAndUpdate(
-                    { _id: params.pizzaId },
-                    //When you add data into a nested array of a MongoDB document, they
-                    // become what's known as a "nested document" or "subdocument".
-                    { $push: { comments: _id } },
-                    { new: true }
-                );
-            })
-            .then(dbPizzaData => {
-                if (!dbPizzaData) {
-                    res.status(404).json({ message: 'No pizza found with this id!' });
-                    return;
-                }
-                res.json(dbPizzaData);
-            })
-            .catch(err => res.json(err));
-    },
+        return Pizza.findOneAndUpdate(
+          { _id: params.pizzaId },
+          { $pull: { comments: params.commentId } },
+          { new: true }
+        );
+      })
+      .then((dbPizzaData) => {
+        if (!dbPizzaData) {
+          res.status(404).json({ message: "No pizza found with this id!" });
+          return;
+        }
+        res.json(dbPizzaData);
+      })
+      .catch((err) => res.json(err));
+  },
 
-    // remove comment
-    removeComment({ params }, res) {
-        Comment.findOneAndDelete({
-            _id: params.commentId
-        })
-            .then(deletedComment => {
-                if (!deletedComment) {
-                    return res.status(404).json({ message: 'No comment with this id!' });
-                }
+  // add reply to a comment
+  addReply({ params, body }, res) {
+    Comment.findOneAndUpdate(
+      { _id: params.commentId },
+      { $push: { replies: body } },
+      { new: true }
+    )
+      .then((dbPizzaData) => {
+        if (!dbPizzaData) {
+          res.status(404).json({ message: "No pizza found with this id!" });
+          return;
+        }
+        res.json(dbPizzaData);
+      })
+      .catch((err) => res.json(err));
+  },
 
-                return Pizza.findOneAndUpdate(
-                    { _id: params.pizzaId },
-                    { $pull: { comments: params.commentId } },
-                    { new: true }
-                );
-            })
-            .then(dbPizzaData => {
-                if (!dbPizzaData) {
-                    res.status(404).json({ message: 'No pizza found with this id!' });
-                    return;
-                }
-                res.json(dbPizzaData);
-            })
-            .catch(err => res.json(err));
-    }
+  // remove reply
+  removeReply({ params }, res) {
+    Comment.findOneAndUpdate(
+      { _id: params.commentId },
+      { $pull: { replies: { replyId: params.replyId } } },
+      { new: true }
+    )
+      .then((dbPizzaData) => res.json(dbPizzaData))
+      .catch((err) => res.json(err));
+  },
 };
 
 module.exports = CommentController;
--- routes/api/comment-routes.js
+++ routes/api/comment-routes.js
@@ -1,17 +1,23 @@
-const router = require('express').Router();
+const router = require("express").Router();
 
 // import the functionality from comments-controller
-const { 
-    addComment, 
-    removeComment 
-} = require('../../controllers/comments-controller');
+const {
+  addComment,
+  removeComment,
+  addReply,
+  removeReply,
+} = require("../../controllers/comments-controller");
 
 // const { remove } = require('../../models/Comment');
 
 // Set up /api/comments/:pizzaId
-router.route('/:pizzaId').post(addComment);
+router.route("/:pizzaId").post(addComment);
 
 // Set up /api/comments/:pizzaId/:commentId
-router.route('/:pizzaId/:commentId').delete(removeComment);
+router.route("/:pizzaId/:commentId").put(addReply).delete(removeComment);
+
+// deleting a reply from a comment will require a new, separate route
+router.route("/:pizzaId/:commentId/:replyId").delete(removeReply);
+
 
 module.exports = router;
