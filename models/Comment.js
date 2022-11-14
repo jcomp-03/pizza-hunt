4ff75686bd4d95028f8fbb44d553c822898ad1b3
jcomp-03@James-Desktop <jcomp_03@yahoo.com>
2022-11-14 16:19:40 -0500

    Added virtuals to Pizza and Comment models.

--- models/Comment.js
+++ models/Comment.js
@@ -1,21 +1,67 @@
 // import required dependencies Schema constructor and model function
-const { Schema, model } = require('mongoose');
+const { Schema, model, Types } = require("mongoose");
+// import function for adjusting createdAt value
+const dateFormat = require("../utils/dateFormat");
 
-const CommentSchema = new Schema({
-    writtenBy: {
-      type: String
+const ReplySchema = new Schema(
+  {
+    // set custom id to avoid confusion with parent comment _id
+    replyId: {
+      type: Schema.Types.ObjectId,
+      default: () => new Types.ObjectId(),
     },
-    commentBody: {
-      type: String
+    replyBody: {
+      type: String,
+    },
+    writtenBy: {
+      type: String,
     },
     createdAt: {
       type: Date,
-      default: Date.now
+      default: Date.now,
+      get: (createdAtVal) => dateFormat(createdAtVal),
+    },
+  },
+  {
+    toJSON: {
+      getters: true,
     }
-});
+  }
+);
 
-// create the Pizza model using the PizzaSchema
-const Comment = model('Comment', CommentSchema);
+const CommentSchema = new Schema(
+  {
+    writtenBy: {
+      type: String,
+    },
+    commentBody: {
+      type: String,
+    },
+    createdAt: {
+      type: Date,
+      default: Date.now,
+      get: (createdAtVal) => dateFormat(createdAtVal),
+    },
+    // unlike our relationship between pizza
+    // and comment data, replies will be nested
+    // directly in a comment's document and not
+    // referred to
+    replies: [ReplySchema],
+  },
+  {
+    toJSON: {
+      virtuals: true,
+      getters: true,
+    }
+  }
+);
+
+CommentSchema.virtual('replyCount').get(function() {
+  return this.replies.length;
+})
+
+// create the Comment model using the CommentSchema
+const Comment = model("Comment", CommentSchema);
 
 // export the Comment model
 module.exports = Comment;
