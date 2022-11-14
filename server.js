004c424fab9a8336dca37714fc4bdb725a0b4aad
jcomp-03@James-Desktop <jcomp_03@yahoo.com>
2022-11-13 20:36:43 -0500

    Included event handling for errors and disconnects on MongoDB database

--- server.js
+++ server.js
@@ -1,21 +1,43 @@
-const mongoose = require('mongoose');
-const express = require('express');
+const mongoose = require("mongoose");
+const express = require("express");
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(express.static('public'));
+app.use(express.static("public"));
 
-app.use(require('./routes'));
+app.use(require("./routes"));
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/pizza-hunt', {
-  useNewUrlParser: true,
-  useUnifiedTopology: true
+
+
+mongoose.connect(
+  process.env.MONGODB_URI || "mongodb://127.0.0.1:27017/pizza-hunt",
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  }
+).catch((error) => {
+  // listen for errors on initial connection
+  console.log('***** Error on initial connection *****');
+  console.log(error);
 });
 
-// Use this to log mongo queries being executed!
-mongoose.set('debug', true);
+// listen for errors after initial connection is established
+mongoose.connection.on('error', error => {
+  console.log('***** Error after initial connection established *****');
+  console.log(error);
+});
 
-app.listen(PORT, () => console.log(`🌍 Connected on localhost:${PORT}`));
+// listen for disconnected event
+mongoose.connection.on('disconnected', error => {
+  console.log('***** Disconnected event *****');
+  console.log(error);
+});
+
+
+// Use this to log mongo queries being executed!
+mongoose.set("debug", true);
+
+app.listen(PORT, () => console.log(`***** 🌍 Connected on localhost:${PORT} *****`));
